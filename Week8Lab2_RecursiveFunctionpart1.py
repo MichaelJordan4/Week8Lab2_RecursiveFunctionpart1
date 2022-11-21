e23df7a0fa57d891c2e95efd1267a45b2afa6bfc
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-11-21 14:16:45 -0800

    Finish Fibonacci

--- Week8Lab2_RecursiveFunctionpart1.py
+++ Week8Lab2_RecursiveFunctionpart1.py
@@ -1,1 +1,21 @@
+#Michael Jordan
+#CIS261
+#Week8Lab2 Fibonacci Recursive Function
+
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+def main():
+    for i in range(16):
+        print(fibonacci(i), end=", ")
+    print("...")
+
+if __name__ == "__main__":
+    main()
 
